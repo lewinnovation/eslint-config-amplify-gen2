3601f1bb52d355514014b950fec73e9ea16a62f7
Vidur Butalia <vidurbutalia@gmail.com>
2025-07-08 16:52:26 +0530

    feat: implement shared eslint config

---
+++ index.js
@@ -0,0 +1,23 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import eslintCdkPlugin from "eslint-cdk-plugin";
+import eslintConfigPrettier from "eslint-config-prettier";
+import perfectionist from "eslint-plugin-perfectionist";
+import tseslint from "typescript-eslint";
+
+export default [
+  {
+    ...eslint.configs.recommended,
+  },
+  perfectionist.configs["recommended-natural"],
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+  {
+    files: ["amplify/backend.ts", "**/resource.ts", "**/*.construct.ts"],
+    extends: [eslintCdkPlugin.configs.recommended],
+    rules: {
+      "@typescript-eslint/no-unused-vars": 0,
+    },
+  },
+];
